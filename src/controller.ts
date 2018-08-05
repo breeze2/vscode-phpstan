57fa0cc2ae6cd082bcdf015ee56c41a94aa10cb4
Breeze <805511939@qq.com>
2018-08-05 23:50:02 +0800

    [*] find configuration & autoload_file
    find configuration & autoload_file

--- src/controller.ts
+++ src/controller.ts
@@ -1,6 +1,7 @@
 import * as child_process from 'child_process';
 import * as vscode from 'vscode';
-// import { basename } from 'path';
+import * as path from 'path';
+import * as fs from 'fs';
 
 interface PhpStanOuput {
   totals: {
@@ -10,22 +11,30 @@ interface PhpStanOuput {
   files: {
     [propName: string]: {
       error: number;
-      messages: PhpStanOutputFileMessage[];
+      messages: {
+        message: string;
+        line: number | null;
+        ignorable: boolean;
+      }[];
     };
   };
   errors: any;
 }
 
-interface PhpStanOutputFileMessage {
-  message: string;
-  line: number|null;
-  ignorable: boolean;
+interface PhpStanArgs {
+  autoload_file?: string;
+  configuration?: string;
+  level?: number|string;
+  memory_limit?: string;
+  no_progress?: boolean;
+  path?: string;
 }
 
 export class PhpStanController {
   static is_analysing: boolean = false;
   protected _phpstan: string = 'phpstan';
   protected _diagnosticCollection: vscode.DiagnosticCollection;
+  protected _worksapce: string = '';
 
   public constructor() {
     this._diagnosticCollection = vscode.languages.createDiagnosticCollection("error");
@@ -51,19 +60,20 @@ export class PhpStanController {
         let uri = vscode.Uri.parse(file_uri);
         output_messages.forEach(el => {
           if (el.line) {
+            let line = el.line - 1;
             let range:vscode.Range;
             let message = el.message;
             if (document && document.uri.toString() === file_uri) {
-              range = new vscode.Range(el.line - 1, 0, el.line - 1, document.lineAt(el.line - 1).range.end.character + 1);
+              range = new vscode.Range(line, 0, line, document.lineAt(line).range.end.character + 1);
               let text = document.getText(range);
               let result = /^(\s*).*(\s*)$/.exec(text);
               if(result) {
-                range = new vscode.Range(el.line - 1, result[1].length, el.line-1, text.length-result[2].length);
+                range = new vscode.Range(line, result[1].length, line, text.length-result[2].length);
               } else {
-                range = new vscode.Range(el.line-1, 0, el.line-1, 1);
+                range = new vscode.Range(line, 0, line, 1);
               }
             } else {
-              range = new vscode.Range(el.line - 1, 0, el.line - 1, 1);
+              range = new vscode.Range(line, 0, line, 1);
             }
             diagnostics.push(new vscode.Diagnostic(range, message));
           }
@@ -73,16 +83,25 @@ export class PhpStanController {
     }
   }
 
-  protected analyse(file: string) {
-    let args = [];
-    let options: {cwd?: string} = {};
+  protected analyse(the_path: string) {
+    let args:PhpStanArgs = {path: the_path};
+    let cwd: string = '';
+    let options:{cwd?: string} = {};
+    args.configuration = this.upFindConfiguration(the_path);
+    if (args.configuration) {
+      cwd = path.dirname(args.configuration);
+    } else {
+      args.autoload_file = this.upFindAutoLoadFile(the_path);
+    }
+    if(args.autoload_file) {
+      cwd = path.dirname(args.autoload_file);
+      cwd = path.dirname(cwd);
+    }
+    if(cwd) {
+      options.cwd = cwd;
+    }
     let that = this;
-    args.push('analyse');
-    args.push('--level=max');
-    args.push('--errorFormat=json');
-    args.push(file);
-    options.cwd = '/Users/Breeze/Workspace/Laravel/lumen5.6';
-    let phpstan = child_process.spawn(this._phpstan, args, options);
+    let phpstan = child_process.spawn(this._phpstan, this.makeCommandArgs(args), options);
     let result = '';
     phpstan.stderr.on('data', (data) => {
       console.log(`stderr: ${data}`);
@@ -99,12 +118,79 @@ export class PhpStanController {
     });
   }
 
-  protected findAutoLoadFile(file: string) {
+  protected makeCommandArgs(args: PhpStanArgs) {
+    let result: string[] = [];
+    result.push('analyse');
+    result.push('--errorFormat=json');
+    result.push('--level=max');
+    if (args.level) {
+      result.push('--level=' + args.level);
+    }
+    if (args.no_progress) {
+      result.push('--no-progress');
+    }
+    if(args.memory_limit) {
+      result.push('--memory-limit=' + args.memory_limit);
+    }
+    if(args.configuration) {
+      result.push('--configuration=' + args.configuration);
+    }
+    if (args.autoload_file) {
+      result.push('--autoload-file=' + args.autoload_file);
+    }
+    if (args.path) {
+      result.push(args.path);
+    }
+    console.log(result);
+    return result;
+  }
+
+  protected setCommandOptions(){
 
   }
 
-  protected findConfigFile(file: string) {
-
+  protected upFindAutoLoadFile(basedir: string) {
+    let basename: string;
+    let parentname: string;
+    let autoload: string;
+    basename = basedir;
+    parentname = path.dirname(basename);
+    autoload = path.join(basename, 'vendor/autoload.php');
+    while (1) {
+      if (fs.existsSync(autoload)) {
+        return autoload;
+      } else if (basename === parentname) {
+        return '';
+      } else {
+        basename = parentname;
+        parentname = path.dirname(basename);
+        autoload = path.join(basename, 'vendor/autoload.php');
+      }
+    }
   }
 
+  protected upFindConfiguration(basedir: string) {
+    let basename: string;
+    let parentname:string;
+    let config1: string;
+    let config2: string;
+    basename = basedir;
+    parentname = path.dirname(basename);
+    config1 = path.join(basename, 'phpstan.neon');
+    config2 = path.join(basename, 'phpstan.neon.dist');
+    while(1) {
+      if(fs.existsSync(config1)) {
+        return config1;
+      } else if(fs.existsSync(config2)) {
+        return config2;
+      } else if(basename === parentname) {
+        return '';
+      } else {
+        basename = parentname;
+        parentname = path.dirname(basename);
+        config1 = path.join(basename, 'phpstan.neon');
+        config2 = path.join(basename, 'phpstan.neon.dist');
+      }
+    }
+  }
 }
